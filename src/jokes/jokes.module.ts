ef1dbee58d64dc22d7952f028efe4a6a7f80dbe0
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2024-11-28 14:02:13 +0530

    Integrate MongoDB connection using MongooseModule.

--- src/jokes/jokes.module.ts
+++ src/jokes/jokes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JokesController } from './jokes.controller';
 import { JokesService } from './jokes.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Joke, JokeSchema } from './schemas/joke.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Joke.name, schema: JokeSchema}])],
   controllers: [JokesController],
   providers: [JokesService]
 })
