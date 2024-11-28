aa1d17afdb924b4c5af98a9fab09ab2303957bce
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2024-11-28 13:45:58 +0530

    Implement basic structure of methods for joke creation and retrieval.

--- src/jokes/jokes.service.ts
+++ src/jokes/jokes.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Joke } from './schemas/joke.schema';
+import { Model } from 'mongoose';
+import { CreateJokeDto } from './dto/create-joke.dto';
+import { JokeStatus } from './enums/joke-status.enum';
 
 @Injectable()
-export class JokesService {}
+export class JokesService {
+    constructor(@InjectModel(Joke.name) private jokeModel: Model<Joke>) {}
+
+    async createJoke(createJokeDto: CreateJokeDto): Promise<Joke> {
+        const createdJoke = new this.jokeModel(createJokeDto);
+        return createdJoke.save();
+    }
+
+    async findAllJokes(): Promise<Joke[]> {
+        return this.jokeModel.find().exec();
+    }
+
+    async findPendingJokes(): Promise<Joke[]> {
+        return this.jokeModel.find({status: JokeStatus.PENDING}).exec();
+    }
+}
